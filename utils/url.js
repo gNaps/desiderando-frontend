bfd937e4f01a8c77ef087d22350962b86f1b5905
Gabriele Napoli <gabrielenapoli@MacBook-Pro-di-Gabriele.local>
2021-03-13 21:14:08 +0100

    add recovery password and sso google

--- utils/url.js
+++ utils/url.js
@@ -1,2 +1,2 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
-export const SSO_GOOGLE_URL = process.env.NEXT_PUBLIC_SSO_GOOGLE_URL || 'https://8b14c8cb97f9.ngrok.io';
+export const SSO_GOOGLE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://8b14c8cb97f9.ngrok.io';
